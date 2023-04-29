32b0068938946e7d500c677ecd9a08585d4e5d7b
Ewa Olczak <ewa.olczak@gmail.com>
2023-04-29 23:08:43 +0200

    Add adding image

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
 
 const app = express();
 
@@ -8,11 +9,25 @@ app.engine(
   'hbs',
   hbs({ extname: 'hbs', layoutsDir: 'views/layouts', defaultLayout: 'main' })
 );
-app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: 'views/layouts' }));
+// app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: 'views/layouts' }));
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/uploads', express.static('/uploads'));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -38,10 +53,10 @@ app.get('/hello/:name', (req, res) => {
   res.render('hello', { layout: false, name: req.params.name });
 });
 
-app.post('/contact/send-message', (req, res) => {
-  const { author, sender, title, message, image } = req.body;
+app.post('/contact/send-message', upload.single('image'), (req, res) => {
+  const { author, sender, title, message } = req.body;
 
-  if (author && sender && title && message && image) {
+  if (author && sender && title && message && req.file) {
     res.render('contact', { isSent: true, filename: req.file.originalname });
   } else {
     res.render('contact', { isError: true });
